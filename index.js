a6e82718c41a73b68b3df682dd20e16ce19d7131
chinna0107 <kancharlahemanth89@gmail.com>
2026-03-27 18:14:09 +0530

    Production 2.0

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+// app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+      "https://alurisglobaltrade.com"
+    ],
+    credentials: true,
+  })
+);
+
 
 app.use('/api/products', require('./routes/products'));
 app.use('/api/admin', require('./routes/admin'));
